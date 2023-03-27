3d7f55bba9d9052b52e512a1485295aa29f8f5b1
Jorge Marquez <104573069+jm181987@users.noreply.github.com>
2023-03-27 18:02:39 +0000

    agregando coordenada en px

--- java.js
+++ java.js
@@ -7,3 +7,18 @@ document.addEventListener("mousemove", function (event) {
   lineX.style.top = event.clientY + "px";
   lineY.style.left = event.clientX + "px";
 });
+
+
+
+// Obtener el elemento que muestra la posición
+var posicionElemento = document.getElementById("posicion");
+
+// Agregar un evento de escucha para detectar el movimiento del mouse
+document.addEventListener("mousemove", function (event) {
+  // Obtener la posición del mouse en la página
+  var x = event.clientX + "px";
+  var y = event.clientY ;
+  
+  // Actualizar el texto del elemento con la posición del mouse
+  posicionElemento.textContent = x + ", " + y + "px";
+});
