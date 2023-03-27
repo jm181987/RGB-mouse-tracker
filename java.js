5c8c523fafd0c41f0854a61c57e633e16342e1ee
Jorge Marquez <104573069+jm181987@users.noreply.github.com>
2023-03-27 19:01:34 +0000

    agregando coordenada en px

--- java.js
+++ java.js
@@ -26,29 +26,3 @@ document.addEventListener("mousemove", function (event) {
 
 
 
-
-// Obtener el modal
-var modal = document.getElementById("myModal");
-
-// Obtener el botón que abre el modal
-var btn = document.getElementById("myBtn");
-
-// Obtener el botón de cierre
-var span = document.getElementsByClassName("close")[0];
-
-// Cuando el usuario haga clic en el botón, abre el modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// Cuando el usuario haga clic en el botón de cierre, cierra el modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Cuando el usuario haga clic fuera del modal, ciérralo
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
